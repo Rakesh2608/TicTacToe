5e57a9e61d1a1eeb53eaa1f72a7e1ea41cff162b
Rakesh2608 <rakeshkumar1432u@gmail.com>
2023-08-05 09:17:51 +0530

    Initial commit

---
+++ src/main/java/org/example/models/Bot.java
@@ -0,0 +1,21 @@
+package org.example.models;
+
+public class Bot extends Player {
+   
+	
+
+	private BotDifficultyLevel botDifficultyLevel;
+	 public Bot(char symbol, String name ,BotDifficultyLevel botDifficultyLevel) {
+			super(symbol, name, PlayerType.BOT);
+			this.botDifficultyLevel=botDifficultyLevel;
+
+		}
+
+    public BotDifficultyLevel getBotDifficultyLevel() {
+        return botDifficultyLevel;
+    }
+
+    public void setBotDifficultyLevel(BotDifficultyLevel botDifficultyLevel) {
+        this.botDifficultyLevel = botDifficultyLevel;
+    }
+}
